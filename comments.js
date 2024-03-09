97b2cee78d61e972e19053d3b8bf7bddbe2058ea
anshul276 <150841677+anshul276@users.noreply.github.com>
2024-03-09 07:12:46 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,34 +1,14 @@
 //create web server 
-var express = require('express');
-var app = express();
-var url = require('url');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var path = require('path');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const http = require('http');
+const fs = require('fs');
 
-//read the comments from the file
-app.get('/comments', function(req, res) {
-    fs.readFile('comments.json', 'utf8', function(err, data) {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(data);
-    });
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' });
+
+    const readStream = fs.createReadStream('./views/index.html');
+    readStream.pipe(res);
 });
 
-//write the comments to the file
-app.post('/comments', function(req, res) {
-    var comment = req.body;
-    fs.readFile('comments.json', 'utf8', function(err, data) {
-        var comments = JSON.parse(data);
-        comments.push(comment);
-        fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-            res.setHeader('Content-Type', 'application/json');
-            res.setHeader('Cache-Control', 'no-cache');
-            res.send(JSON.stringify(comments));
-        });
-    });
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
 });
-
-app.listen(3000);
-console.log('Server started: http://localhost:3000/');
