a385336cfa1c84906f20cda824076234a51cd9e3
anshul276 <150841677+anshul276@users.noreply.github.com>
2024-03-09 07:18:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,12 @@
 //create web server 
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' });
-
-    const readStream = fs.createReadStream('./views/index.html');
-    readStream.pipe(res);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello, World!');
+    res.end();
 });
 
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+server.listen(8080, () => {
+    console.log('Server is running at http://localhost:8080/');
 });
